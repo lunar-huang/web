52712e43167d92fb9efff2ca268b532f315cf3f4
LunarHuang <lunarhuang@outlook.com>
2024-09-04 17:43:12 +0800

    Create init.sql

---
+++ assignment-2/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE user_record;
+
+USE user_record;
+
+CREATE TABLE users (    id INT AUTO_INCREMENT PRIMARY KEY,    username VARCHAR(255) NOT NULL UNIQUE,    password VARCHAR(255) NOT NULL,    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);，
+
